6bcbfb93e99d6a16a0c09ac7e1ef83b7860fe41f
joket1999 <joshuabetter@hotmail.com>
2022-12-29 23:12:08 +0800

    none

--- config/dev.js
+++ config/dev.js
@@ -1,13 +1,9 @@
 const ENV = process.env.TARO_ENV;
-let outputRoot = ENV === 'weapp' ? './build/weapp' : './build/tt';
-// let outputRoot = ENV === 'weapp' ? '../../../../../../mnt/hgfs/buy-ticket/dist' : '../../../../../../mnt/hgfs/buy-ticket/ticket';
 module.exports = {
 	env: {
 		NODE_ENV: '"development"'
 	},
-	outputRoot,
-	defineConstants: {
-	},
+	defineConstants: {},
 	mini: {},
 	h5: {}
 }
